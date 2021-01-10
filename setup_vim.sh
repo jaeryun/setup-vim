dd9617074dccb2a4379341d215dcc201d0dd0293
Jaeryun Lee <jrlee@kakao.com>
2021-01-10 21:11:18 +0900

    Update setup_vim.sh

--- setup_vim.sh
+++ setup_vim.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 cp ./.vimrc ~/
 apt install ctags && echo "alias ctags=/usr/local/bin/ctags" >> ~/.profile
