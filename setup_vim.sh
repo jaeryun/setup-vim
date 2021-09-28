8b49af5c07a41b7c1208e7da681c0905dae9c63c
Jaeryun Lee <jrlee@kakao.com>
2021-09-29 01:11:02 +0900

    Update setup_vim.sh

--- setup_vim.sh
+++ setup_vim.sh
@@ -4,3 +4,4 @@ sudo apt install ctags && echo "alias ctags=/usr/local/bin/ctags" >> ~/.profile
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 cp ./.vimrc ~/
 vim -c PlugInstall
+vim -c GoInstallBinaries
