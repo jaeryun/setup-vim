73eb16d912d9fa8f54a5d1ec1fd67a0089252465
Jaeryun Lee <jrlee@kakao.com>
2026-03-11 00:16:02 +0900

    Update setup_vim.sh to simplify setup process
    Removed installation of ctags and Go binaries.

--- setup_vim.sh
+++ setup_vim.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-sudo apt install ctags && echo "alias ctags=/usr/local/bin/ctags" >> ~/.profile
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 cp ./.vimrc ~/
 vim -c PlugInstall
-vim -c GoInstallBinaries
